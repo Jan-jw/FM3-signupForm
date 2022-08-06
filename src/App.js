97b8e0707268114e14393f3d5e108b1a718ef57c
Jan-jw <74130412+Jan-jw@users.noreply.github.com>
2022-08-06 01:03:13 -0400

    left part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-// import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg"> 
+      <div className="slogan">
+      <h1> Learn to code by watching others</h1>
+      <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+          but understanding how developers think is invaluable.</p>
+      </div>
+
+      <div className="form">
+        <button className="form-trial">
+
+        </button>
+        <div  className="form-fill">
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
